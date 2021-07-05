453ff8df5f6f6f2711410128b35f687297dfc214
Hiyen <lightpromise89@gmail.com>
2021-07-06 00:14:53 +0900

    add pun2
    apply pun2
    connect socketNetworkt
    connect photon Server

---
+++ M_Portfolio/Assets/Scripts/Network/PhotonNetworkSystem.cs
@@ -0,0 +1,33 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+
+namespace Network
+{
+    public class PhotonNetworkSystem : MonoBehaviourPunCallbacks
+    {
+        string _networkState;
+        void Start()
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+		public override void OnConnectedToMaster()
+		{
+			base.OnConnectedToMaster();
+            PhotonNetwork.JoinLobby();
+		}
+		public override void OnJoinedLobby()
+		{
+			base.OnJoinedLobby();
+			PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions { MaxPlayers = 4 }, null);
+		}
+		void Update()
+		{
+			string currNetworkState = PhotonNetwork.NetworkClientState.ToString();
+			if(_networkState != currNetworkState)
+			{
+				_networkState = currNetworkState;
+				print(_networkState);
+			}
+		}
+	}
+}
---
+++ M_Portfolio/Assets/Scripts/Network/SocketNetworkSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+namespace Network
+{
+	public class SocketNetworkSystem : MonoBehaviour
+	{
+		public void Start()
+		{
+			IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5959);
+			Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			socket.Connect(endpoint);
+
+			byte[] data = new byte[1024];
+			socket.Receive(data);
+			string buffer;// = Encoding.Default.GetString(data);
+
+			buffer = "Receive Success";
+			data = Encoding.Default.GetBytes(buffer);
+			socket.Send(data);
+			socket.Close();
+		}		
+	}
+}
